970137f44f1afe43b65f4f6ec3d0c9c053845fcc
small-dogg <sy920112@naver.com>
2022-05-15 00:17:07 +0900

    feat: API 예외 처리   - HandlerExceptionResolver를 사용한 내부에서 Exception 처리   - 스프링이 제공하는 ExceptionResolver를 사용한 내부에서 Exception 처리   - 애너테이션으로 쉽게 구현한 Exception 처리   - Advice 애너테이션을 사용한 Exception 공통처리

---
+++ src/main/java/com/smalldogg/study/exception/api/ApiExceptionV2Controller.java
@@ -0,0 +1,41 @@
+package com.smalldogg.study.exception.api;
+
+import com.smalldogg.study.exception.exception.UserException;
+import com.smalldogg.study.exception.exhandler.ErrorResult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api2")
+public class ApiExceptionV2Controller {
+
+
+
+    @GetMapping("/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+
+}
